3b86ede7f5244299f6b84ae91a8e1766b1473ef9
johnsmith0x3f <johnsmith0x3f@gmail.com>
2024-08-18 06:47:25 +0000

    Participate in CF2004.

--- Codeforces/contest/2004/C.cpp
+++ Codeforces/contest/2004/C.cpp
@@ -4,7 +4,7 @@ using namespace std;
 using i64 = long long;
 
 template <typename T = int>
-constexpr read() {
+constexpr T read() {
 	T x = 0; bool f = 1; char c = getchar();
 	while(isdigit(c) ^ 1) f &= (c != 45), c = getchar();
 	while(isdigit(c)) x = (x << 1) + (x << 3) + (c ^ 48), c = getchar();
--- Codeforces/contest/2004/E.cpp
+++ Codeforces/contest/2004/E.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 template <typename T = int>
-constexpr read() {
+constexpr T read() {
 	T x = 0; bool f = 1; char c = getchar();
 	while(isdigit(c) ^ 1) f &= (c != 45), c = getchar();
 	while(isdigit(c)) x = (x << 1) + (x << 3) + (c ^ 48), c = getchar();
