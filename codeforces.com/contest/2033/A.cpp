15773291ff2b07d70817536e6fbe471e406f9047
johnsmith0x3f <johnsmith0x3f@gmail.com>
2024-10-27 04:08:37 +0000

    Solve A.

---
+++ codeforces.com/contest/2033/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int tt;
+	cin >> tt;
+	while(tt--) {
+		int n;
+		cin >> n;
+
+		cout << (n & 1 ? "Kosuke" : "Sakurako") << "\n";
+	}
+
+	return 0;
+}
