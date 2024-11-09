58624d1a6c324dec18bfb87712150f269be3407b
John Smith <johnsmith0x3f@gmail.com>
2024-11-09 18:18:58 +0000

    Solve E.

---
+++ codeforces.com/contest/2029/E.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 4e5 + 5;
+int tot, d[N], pr[N];
+bool isprime[N];
+
+inline void sieve() {
+	for(int i = 2; i < N; ++i) isprime[i] = 1;
+
+	for(int i = 2; i < N; ++i) {
+		if(isprime[i]) pr[++tot] = d[i] = i;
+		for(int j = 1; j <= tot && i * pr[j] < N; ++j) {
+			isprime[i * pr[j]] = 0, d[i * pr[j]] = pr[j];
+			if(i % pr[j] == 0) break;
+		}
+	}
+}
+
+inline void solve() {
+	int n;
+	cin >> n;
+
+	vector<int> a(n);
+	for(int i = 0; i < n; ++i) cin >> a[i];
+
+	int x = 2;
+	for(int i = 0; i < n; ++i)
+		if(isprime[a[i]]) x = a[i];
+
+	for(int i = 0; i < n; ++i)
+		if(a[i] != x && a[i] - (a[i] & 1) * d[a[i]] < 2 * x) x = -1;
+
+	cout << x << "\n";
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+
+	sieve();
+
+	int t;
+	cin >> t;
+	while(t--) solve();
+
+	return 0;
+}
