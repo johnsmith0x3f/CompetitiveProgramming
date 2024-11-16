8a55fd12ed8b7b3a7f6ea2409fe3b2c4d07aa269
John Smith <johnsmith0x3f@gmail.com>
2024-11-16 05:57:44 +0000

    Update C.

--- codeforces.com/contest/2031/C.cpp
+++ codeforces.com/contest/2031/C.cpp
@@ -1,8 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const string str = "1 2 2 3 3 4 4 5 5 1 6 6 7 7 8 8 9 9 10 10 11 11 13 12 12 1 13 ";
-
 inline void solve() {
 	int n;
 	cin >> n;
@@ -13,10 +11,12 @@ inline void solve() {
 			return;
 		}
 		n -= 27;
-		cout << str;
+		cout << "1 2 2 3 3 4 4 5 5 1 6 6 7 7 8 8 9 9 10 10 11 11 13 12 12 1 13";
 	}
+	else n -= 2, cout << "1 1";
 
-	for(int i = 1; i <= n / 2; ++i) cout << i + 100 << ' ' << i + 100 << " \n"[i == n / 2];
+	for(int i = 1; i <= n / 2; ++i) cout << ' ' << i + 100 << ' ' << i + 100;
+	cout << '\n';
 }
 
 int main() {
