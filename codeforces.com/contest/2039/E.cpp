68147b12fbf40c78740d1f4fc0d2f4d1c0fc3ec6
John Smith <johnsmith0x3f@gmail.com>
2024-11-25 02:25:24 +0000

    Update E.

--- codeforces.com/contest/2039/E.cpp
+++ codeforces.com/contest/2039/E.cpp
@@ -3,32 +3,34 @@ using namespace std;
 
 using i64 = long long;
 
+const int N = 1e6 + 5;
 const int P = 998244353;
+i64 dp[N], ans[N];
 
-inline void solve() {
-	int n;
-	cin >> n;
-
-	vector<i64> dp(n + 1);
-
+inline void init(int n) {
 	i64 pre = 0;
 	for(int i = 3; i <= n; ++i) {
 		dp[i] = (pre + 1) % P;
 		pre = (pre + dp[i] * i - 1) % P;
 	}
 
-	i64 ans = 1;
-	for(int i = 3; i <= n; ++i) ans += dp[i];
-	cout << ans % P << '\n';
+	ans[1] = 0, ans[2] = 1;
+	for(int i = 3; i <= n; ++i) ans[i] = (ans[i - 1] + dp[i]) % P;
 }
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
+	init(1e6);
+
 	int t;
 	cin >> t;
-	while(t--) solve();
+	while(t--) {
+		int n;
+		cin >> n;
+		cout << ans[n] << '\n';
+	}
 
 	return 0;
 }
