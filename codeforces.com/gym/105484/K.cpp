f9c84fefbdb0737b3fd58709a4c39710e532bb13
John Smith <johnsmith0x3f@gmail.com>
2024-11-27 17:39:57 +0000

    Try K.

---
+++ codeforces.com/gym/105484/K.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+inline void solve() {
+	int n, m, k, w;
+	cin >> n >> m >> k >> w;
+
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t;
+	cin >> t;
+	while(t--) solve();
+
+	return 0;
+}
