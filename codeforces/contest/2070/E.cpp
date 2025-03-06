ea82e2fa2831846e44a017ee3760c4bab285aa60
John Smith <johnsmith0x3f@gmail.com>
2025-03-06 09:23:42 +0000

    Update E.

--- codeforces/contest/2070/E.cpp
+++ codeforces/contest/2070/E.cpp
@@ -7,15 +7,29 @@ using i64 = long long;
 const int N = 5e6 + 5;
 int c[N];
 
-void update(int x) {
-	while(x < N) ++c[x], x += x & -x;
-}
+struct FenwickTree {
+	std::vector<int> tree;
 
-int query(int x) {
-	int ret = 0;
-	while(x) ret += c[x], x -= x & -x;
-	return ret;
-}
+	FenwickTree(int s) {
+		tree.resize(s + 1, 0);
+	}
+
+	void update(int i, int v) {
+		while(i < tree.size()) {
+			tree[i] += v;
+			i += i & -i;
+		}
+	}
+
+	int query(int i) {
+		int sum = 0;
+		while(i > 0) {
+			sum += tree[i];
+			i -= i & -i;
+		}
+		return sum;
+	}
+};
 
 int main() {
 	ios::sync_with_stdio(false);
@@ -27,24 +41,21 @@ int main() {
 	string s;
 	cin >> s;
 
-	vector<int> sum(n + 1);
+	vector<int> sum(n + 1); sum[0] = n * 3;
 	for(int i = 0; i < n; ++i) {
 		if(s[i] == '0') sum[i + 1] = sum[i] + 1;
 		if(s[i] == '1') sum[i + 1] = sum[i] - 3;
 	}
 
 	i64 ans = 0;
+	FenwickTree t(n * 4 + 1);
+
 	for(int i = 1; i <= n; ++i) {
-		// int temp = query(sum[i] + n * 3 + 1);
-		// cout << temp << endl;
-		ans += query(sum[i] + 1e6 + 1) - query(sum[i] + 1e6) + query(sum[i] + 1e6 - 2);
-		update(sum[i - 1] + 1e6);
+		ans += t.query(sum[i] + 1) - t.query(sum[i]) + t.query(sum[i] - 2);
+		t.update(sum[i - 1], 1);
 	}
 
 	cout << ans << '\n';
 
 	return 0;
 }
-/*
- * (n0 + 1) // 3 > n1 || (n0 + 1) / 3 == n1
-*/
